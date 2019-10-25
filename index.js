085ff707d781e1557e1938cf71b8455cbc38ff30
Joakim Loxdal <loxdal@kth.se>
2019-10-25 17:19:48 +0200

    cross origin again

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ var striptags = require('striptags');
 require('dotenv').config()
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
-io.set('origins', '*:*');
-
+// var io = require('socket.io')(server);
+const io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 var path = require('path');
 var port = process.env.PORT || 80;
 
